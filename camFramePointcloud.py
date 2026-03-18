c8f88d2f318d812c2f61c7c8cd89825d4b8e483a
Arthur Calabria Guerra <163882421+SillyGo@users.noreply.github.com>
2026-03-18 07:09:12 -0300

    completando o código

--- camFramePointcloud.py
+++ camFramePointcloud.py
@@ -3,6 +3,16 @@ from sensor_msgs.msg import Image
 import rclpy 
 from rclpy.node import Node
 from cv_bridge import CvBridge
+from std_msgs.msg import Header
+import cv2 
+import numpy as np
+from sensor_msgs_py import point_cloud2 as pc2
+
+#parâmetros da câmera: obti esses valores fazendo ros2 topic echo /lane_camera/camera_info e guardando os resultados.
+fx = 381.3469774171306
+fy = 381.3469774171306
+cx = 320.0
+cy = 240.0
 
 class PointCloudPub(Node):
     def __init__(self):
@@ -37,6 +47,57 @@ class PointCloudPub(Node):
         #8. salva todas essas 3-uplas (x,y,z) em um grande array
         #9. converte esse array para pointcloud 
         #10. publica a pointcloud no frame 'Camera Link'
+
+        DepthMsg = self.last_depth_image # evita a mudança desse valor durante a execução!
+
+        timeMono = msg.header.stamp.nanosec
+        timeDepth = DepthMsg.header.stamp.nanosec
+
+        precision = 500
+
+        if abs(timeMono - timeDepth) >= precision:
+            self.get_logger().warn("ERRO: mensagens não sincronizadas!")
+            return
+        else:
+            self.get_logger().info("INFO: mensagens recebidas estão sincronizadas!")
+
+        MonoImage = self.br.imgmsg_to_cv2(msg, desired_encoding='bgr8') 
+        DepthImage = self.br.imgmsg_to_cv2(DepthMsg, desired_encoding='passthrough') 
+
+        #VI E GARANTO: MonoImage e DepthImage tem as mesmas dimensões.
+
+        #Com isso, todos os testes de segurança deram certo. Daremos prosseguimento à execução do código.
+
+        h, w = MonoImage.shape[:2]
+
+        MonoImage = MonoImage[0:int(5*h/5), :]
+
+        h, w = MonoImage.shape[:2]
+
+        gray = cv2.cvtColor(MonoImage, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray, 50, 100)
+
+        edge_points = np.argwhere(edges > 0)
+
+        rows = edge_points[:, 0]
+        cols = edge_points[:, 1]
+
+        Z = DepthImage[rows, cols]
+
+        X = Z * (cols - cx) / fx
+        Y = Z * (rows - cy) / fy
+
+        global_points = np.stack((X, Y, Z), axis=1)
+
+        header = Header()
+        header.stamp = self.get_clock().now().to_msg()
+        header.frame_id = 'lane_camera_optical_frame'
+
+        cloud_msg = pc2.create_cloud_xyz32(header, global_points)
+        self.pcPub.publish(cloud_msg)
+
+        cv2.imshow('Webcam Display', MonoImage)
+        cv2.waitKey(1)    
 
         return
 
